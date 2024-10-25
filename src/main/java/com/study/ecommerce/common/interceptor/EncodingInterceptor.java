0a0a51d6ff1ee672a84ec2e50d0ed9121352bf80
won taek oh <dnjsxorchlrh@naver.com>
2024-10-25 23:53:48 +0900

    feat: 로깅 및 인코딩 인터셉터 구현 및 추가

--- src/main/java/com/study/ecommerce/common/interceptor/EncodingInterceptor.java
+++ src/main/java/com/study/ecommerce/common/interceptor/EncodingInterceptor.java
@@ -1,10 +1,12 @@
 package com.study.ecommerce.common.interceptor;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class EncodingInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws
